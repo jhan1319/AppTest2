4b5a5b2a2ce92c151c97afe90c0137fa4e9a8cd5
Jhan Almonte <jhanalmonte98@gmail.com>
2021-03-27 19:55:55 -0400

    1

--- src/main/java/edu.pucmm.eict/main.java
+++ src/main/java/edu.pucmm.eict/main.java
@@ -1,16 +1,24 @@
 package edu.pucmm.eict;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.JavalinRenderer;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
 
 public class main {
 
     public static void main(String[] args){
 
+
         Javalin app = Javalin.create(config -> {
+
         }).start(8002);
+
+        JavalinRenderer.register(JavalinThymeleaf.INSTANCE, ".html");
+
+
         app.get("/", ctx -> {
 
-            ctx.result("Bienvenido a aplicacion 2");
+            ctx.result("Bienvenido a aplicacion 2.");
 
 
         });
